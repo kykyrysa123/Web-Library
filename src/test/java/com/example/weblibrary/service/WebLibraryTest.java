fe6bdb5497f2dab9638c242c2c3271c9dbda2f2c
Midlay <stasyukartem02@gmail.com>
2025-03-13 12:05:50 +0300

    fix error in sonar

--- src/test/java/com/example/weblibrary/service/WebLibraryTest.java
+++ src/test/java/com/example/weblibrary/service/WebLibraryTest.java
@@ -1,9 +1,10 @@
 package com.example.weblibrary.service;
 
+import com.example.weblibrary.WebLibraryApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = WebLibraryApplication.class)
 class WebLibraryTest {
 
   @Test
