d89ff0930bcdb752e2bf495a106d428e392eef2c
Midlay <stasyukartem02@gmail.com>
2025-04-01 00:09:11 +0300

    апрощз

--- src/main/java/com/example/weblibrary/model/User.java
+++ src/main/java/com/example/weblibrary/model/User.java
@@ -14,9 +14,6 @@ import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 /**
  * Represents a user in the system.
