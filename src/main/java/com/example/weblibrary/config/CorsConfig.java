5b12025ccbe4a069f7d4859077d07490dd8c365a
Midlay <stasyukartem02@gmail.com>
2025-04-23 22:01:51 +0300

    Denie cors

--- src/main/java/com/example/weblibrary/config/CorsConfig.java
+++ src/main/java/com/example/weblibrary/config/CorsConfig.java
@@ -1,25 +1,22 @@
-/*
 package com.example.weblibrary.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-*/
 /**
  * Configuration class for Cross-Origin Resource Sharing (CORS) settings.
  * Defines allowed origins, methods, headers, and other CORS-related configurations
  * for the application.
- *//*
-
+ */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "http://localhost:80", "https://web-library-production.up.railway.app/")
+            .allowedOrigins("http://localhost:3000", "http://localhost:80", "http://web-library-production.up.railway.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
   }
-}*/
+}
